557988f4d3c9695b8e2c16c49b2fe3b61efce405
Andrey Sitnik <andrey@sitnik.ru>
2016-09-10 20:47:48 +1000

    Use only ES5 in project to be sure in browser build

--- test/create-test-timer.test.js
+++ test/create-test-timer.test.js
@@ -2,14 +2,14 @@ var test = require('ava')
 
 var createTestTimer = require('../create-test-timer')
 
-test('generates uniq time marks', t => {
+test('generates uniq time marks', function (t) {
   var timer = createTestTimer()
   for (var i = 1; i <= 10; i++) {
     t.deepEqual(timer(), [i])
   }
 })
 
-test('starts from same value', t => {
+test('starts from same value', function (t) {
   var timer1 = createTestTimer()
   var timer2 = createTestTimer()
   for (var i = 0; i <= 10; i++) {
