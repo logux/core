f0493458d19ec9310363615ead7a3b50caa09987
Andrey Sitnik <andrey@sitnik.ru>
2016-09-11 14:44:39 +1000

    Use ES5 var instead of ES6 let

--- test/create-timer.test.js
+++ test/create-timer.test.js
@@ -1,19 +1,19 @@
 var createTimer = require('../create-timer')
 
 it('starts events from zero', function () {
-  let timer = createTimer('test')
-  let time = timer()
+  var timer = createTimer('test')
+  var time = timer()
   expect(time[2]).toBe(0)
 })
 
 it('saves host name', function () {
-  let timer = createTimer('host')
-  let time = timer()
+  var timer = createTimer('host')
+  var time = timer()
   expect(time[2]).toBe(0)
 })
 
 it('generates unique time marks', function () {
-  let timer = createTimer('test')
+  var timer = createTimer('test')
   var time1 = timer()
   var time2 = timer()
   expect(time1).not.toEqual(time2)
