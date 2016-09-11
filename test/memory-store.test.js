44d3a9b600a812b652ea6e71b85804e5aaefe2c7
Andrey Sitnik <andrey@sitnik.ru>
2016-09-11 13:10:40 +1000

    Move to Jest and add coverage check to npm test

--- test/memory-store.test.js
+++ test/memory-store.test.js
@@ -1,35 +1,43 @@
-var test = require('ava')
-
 var MemoryStore = require('../memory-store')
 
-function checkContent (t, store, events) {
+function checkContent (store, events) {
   return store.get().then(page => {
-    t.deepEqual(page.data, events)
-    t.deepEqual(typeof page.next, 'undefined')
+    expect(page.data).toEqual(events)
+    expect(page.next).toBeUndefined()
   })
 }
 
-test('is empty in the beginning', function (t) {
+it('is empty in the beginning', function () {
   var store = new MemoryStore()
-  return checkContent(t, store, [])
+  return checkContent(store, [])
 })
 
-test('adds first event', function (t) {
+it('adds first event', function () {
   var store = new MemoryStore()
   store.add({ time: [1] })
-  return checkContent(t, store, [
+  return checkContent(store, [
     { time: [1] }
   ])
 })
 
-test('stores events sorted', function (t) {
+it('stores evewnts with same times', function () {
+  var store = new MemoryStore()
+  store.add({ id: 1, time: [1] })
+  store.add({ id: 2, time: [1] })
+  return checkContent(store, [
+    { id: 1, time: [1] },
+    { id: 2, time: [1] }
+  ])
+})
+
+it('stores events sorted', function () {
   var store = new MemoryStore()
   store.add({ time: [3] })
   store.add({ time: [2] })
   store.add({ time: [5] })
   store.add({ time: [1] })
   store.add({ time: [4] })
-  return checkContent(t, store, [
+  return checkContent(store, [
     { time: [5] },
     { time: [4] },
     { time: [3] },
@@ -38,7 +46,7 @@ test('stores events sorted', function (t) {
   ])
 })
 
-test('sorts events by time as array', function (t) {
+it('sorts events by time as array', function () {
   var store = new MemoryStore()
   store.add({ time: [1, 1, 1] })
   store.add({ time: [2, 1, 1] })
@@ -47,7 +55,7 @@ test('sorts events by time as array', function (t) {
   store.add({ time: [2, 1, 2] })
   store.add({ time: [2, 3, 1] })
   store.add({ time: [3, 1, 1] })
-  return checkContent(t, store, [
+  return checkContent(store, [
     { time: [3, 1, 1] },
     { time: [2, 3, 1] },
     { time: [2, 2, 1] },
