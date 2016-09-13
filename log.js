87ad83fe97f1ad4b193368c164328d71b1e8d216
Andrey Sitnik <andrey@sitnik.ru>
2016-09-13 13:20:53 +1000

    Move abstract JSDoc definitions to the end of file

--- log.js
+++ log.js
@@ -1,60 +1,4 @@
 /**
- * Sortable unique event ID.
- * Array of comparable native types (like number or string).
- *
- * @typedef {array} Time
- *
- * @example
- * [1, 'host']
- */
-
-/**
- * Log’s event.
- *
- * @typedef {object} Event
- * @property {string} type Event type name.
- * @property {Time} [time] Event occured time. {@link Log#add} will fill it,
- *                         if field will be empty.
- */
-
-/**
- * Every log store should provide two methods: add and get.
- *
- * @typedef {object} Store
- * @property {function} add    Add new event to store. Event always will
- *                             have type and time properties.
- * @property {function} get    Return a Promise with events “page”.
- *                             Page is a object with events in `data` property
- *                             and function `next` to return Promise with
- *                             next page. Last page should not have `next`.
- * @property {function} remove Remove event from store
- */
-
-/**
- * Returns current time. Time should be unique for every call.
- *
- * @typedef {function} Timer
- * @return {Time}
- */
-
-/**
- * @callback listener
- * @param {Event} event new event
- */
-
-/**
- * @callback iterator
- * @param {Event} event next event
- * @return {boolean} returning false will stop iteration
- */
-
-/**
- * @callback keeper
- * @param {Event} event next event
- * @return {boolean} true if event should be keeped from cleaning
- */
-
-/**
  * Log is main idea in Logux to store timed events inside.
  *
  * @param {object} opts options
@@ -242,3 +186,59 @@ Log.prototype = {
 }
 
 module.exports = Log
+
+/**
+ * Sortable unique event ID.
+ * Array of comparable native types (like number or string).
+ *
+ * @typedef {array} Time
+ *
+ * @example
+ * [1, 'host']
+ */
+
+/**
+ * Log’s event.
+ *
+ * @typedef {object} Event
+ * @property {string} type Event type name.
+ * @property {Time} [time] Event occured time. {@link Log#add} will fill it,
+ *                         if field will be empty.
+ */
+
+/**
+ * Every log store should provide two methods: add and get.
+ *
+ * @typedef {object} Store
+ * @property {function} add    Add new event to store. Event always will
+ *                             have type and time properties.
+ * @property {function} get    Return a Promise with events “page”.
+ *                             Page is a object with events in `data` property
+ *                             and function `next` to return Promise with
+ *                             next page. Last page should not have `next`.
+ * @property {function} remove Remove event from store
+ */
+
+/**
+ * Returns current time. Time should be unique for every call.
+ *
+ * @typedef {function} Timer
+ * @return {Time}
+ */
+
+/**
+ * @callback listener
+ * @param {Event} event new event
+ */
+
+/**
+ * @callback iterator
+ * @param {Event} event next event
+ * @return {boolean} returning false will stop iteration
+ */
+
+/**
+ * @callback keeper
+ * @param {Event} event next event
+ * @return {boolean} true if event should be keeped from cleaning
+ */
