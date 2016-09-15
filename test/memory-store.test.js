56306172fa2a87c42a4c4d089a0a6689d8c651ab
Andrey Sitnik <andrey@sitnik.ru>
2016-09-15 18:21:46 +1000

    Clean up MemoryStore test

--- test/memory-store.test.js
+++ test/memory-store.test.js
@@ -1,52 +1,50 @@
 var MemoryStore = require('../memory-store')
 
-function checkContent (store, created, added) {
+function checkCreated (store, created) {
   return store.get('created').then(function (page) {
     expect(page.data).toEqual(created)
     expect(page.next).toBeUndefined()
-  }).then(function () {
-    return store.get('added').then(function (page) {
-      expect(page.data).toEqual(added)
-      expect(page.next).toBeUndefined()
-    })
   })
 }
 
 it('is empty in the beginning', function () {
   var store = new MemoryStore()
-  return checkContent(store, [], [])
+  return checkCreated(store, [])
 })
 
 it('adds first event', function () {
   var store = new MemoryStore()
   store.add([{ a: 1 }, { created: [1], added: [1] }])
-  return checkContent(store, [
-    [{ a: 1 }, { created: [1], added: [1] }]
-  ], [
+  return checkCreated(store, [
     [{ a: 1 }, { created: [1], added: [1] }]
   ])
 })
 
 it('stores events sorted', function () {
   var store = new MemoryStore()
-  store.add([{ a: 1 }, { created: [3], added: 2 }])
-  store.add([{ a: 2 }, { created: [2], added: 3 }])
-  store.add([{ a: 3 }, { created: [5], added: 1 }])
+  store.add([{ a: 1 }, { created: [3], added: 1 }])
+  store.add([{ a: 2 }, { created: [2], added: 2 }])
+  store.add([{ a: 3 }, { created: [5], added: 3 }])
   store.add([{ a: 4 }, { created: [1], added: 4 }])
   store.add([{ a: 5 }, { created: [4], added: 5 }])
-  return checkContent(store, [
-    [{ a: 3 }, { created: [5], added: 1 }],
+  return checkCreated(store, [
+    [{ a: 3 }, { created: [5], added: 3 }],
     [{ a: 5 }, { created: [4], added: 5 }],
-    [{ a: 1 }, { created: [3], added: 2 }],
-    [{ a: 2 }, { created: [2], added: 3 }],
+    [{ a: 1 }, { created: [3], added: 1 }],
+    [{ a: 2 }, { created: [2], added: 2 }],
     [{ a: 4 }, { created: [1], added: 4 }]
-  ], [
-    [{ a: 5 }, { created: [4], added: 5 }],
-    [{ a: 4 }, { created: [1], added: 4 }],
-    [{ a: 2 }, { created: [2], added: 3 }],
-    [{ a: 1 }, { created: [3], added: 2 }],
-    [{ a: 3 }, { created: [5], added: 1 }]
-  ])
+  ]).then(function () {
+    return store.get('added').then(function (page) {
+      expect(page.data).toEqual([
+        [{ a: 5 }, { created: [4], added: 5 }],
+        [{ a: 4 }, { created: [1], added: 4 }],
+        [{ a: 3 }, { created: [5], added: 3 }],
+        [{ a: 2 }, { created: [2], added: 2 }],
+        [{ a: 1 }, { created: [3], added: 1 }]
+      ])
+      expect(page.next).toBeUndefined()
+    })
+  })
 })
 
 it('support time array in created', function () {
@@ -58,15 +56,7 @@ it('support time array in created', function () {
   store.add([{ }, { created: [2, 1, 2], added: 3 }])
   store.add([{ }, { created: [2, 3, 1], added: 6 }])
   store.add([{ }, { created: [3, 1, 1], added: 7 }])
-  return checkContent(store, [
-    [{ }, { created: [3, 1, 1], added: 7 }],
-    [{ }, { created: [2, 3, 1], added: 6 }],
-    [{ }, { created: [2, 2, 1], added: 5 }],
-    [{ }, { created: [2, 1, 3], added: 4 }],
-    [{ }, { created: [2, 1, 2], added: 3 }],
-    [{ }, { created: [2, 1, 1], added: 2 }],
-    [{ }, { created: [1, 1, 1], added: 1 }]
-  ], [
+  return checkCreated(store, [
     [{ }, { created: [3, 1, 1], added: 7 }],
     [{ }, { created: [2, 3, 1], added: 6 }],
     [{ }, { created: [2, 2, 1], added: 5 }],
@@ -83,10 +73,7 @@ it('removes events', function () {
   store.add([{ }, { created: [2], added: 2 }])
   store.add([{ }, { created: [3], added: 3 }])
   store.remove([{ }, { created: [2], added: 2 }])
-  return checkContent(store, [
-    [{ }, { created: [3], added: 3 }],
-    [{ }, { created: [1], added: 1 }]
-  ], [
+  return checkCreated(store, [
     [{ }, { created: [3], added: 3 }],
     [{ }, { created: [1], added: 1 }]
   ])
