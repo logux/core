e1d05d2a884a6b9abd16cd275c9d51da7fc68e10
Andrey Sitnik <andrey@sitnik.ru>
2016-09-15 18:05:36 +1000

    Store time in separated meta object ith added order

--- memory-store.js
+++ memory-store.js
@@ -2,6 +2,14 @@ var SortedArray = require('sorted-array')
 
 var compareTime = require('./compare-time')
 
+function compareCreated (a, b) {
+  return compareTime(b[1].created, a[1].created)
+}
+
+function compareAdded (a, b) {
+  return b[1].added - a[1].added
+}
+
 /**
  * Simpliest memory-based events store.
  *
@@ -19,26 +27,27 @@ var compareTime = require('./compare-time')
  * @class
  */
 function MemoryStore () {
-  this.sorted = new SortedArray([], this.compare)
+  this.created = new SortedArray([], compareCreated)
+  this.added = new SortedArray([], compareAdded)
 }
 
 MemoryStore.prototype = {
 
-  get: function get () {
-    return Promise.resolve({ data: this.sorted.array })
+  get: function get (order) {
+    var data = order === 'added' ? this.added : this.created
+    return Promise.resolve({ data: data.array })
   },
 
-  add: function add (event) {
-    this.sorted.insert(event)
+  add: function add (entry) {
+    this.created.insert(entry)
+    this.added.insert(entry)
   },
 
-  remove: function remove (event) {
-    this.sorted.remove(event)
-  },
-
-  compare: function compare (a, b) {
-    return compareTime(b.time, a.time)
+  remove: function remove (entry) {
+    this.created.remove(entry)
+    this.added.remove(entry)
   }
+
 }
 
 module.exports = MemoryStore
