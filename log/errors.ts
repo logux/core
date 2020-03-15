09c92e208b828a893b38b5c31acb44323079a89c
Andrey Sitnik <andrey@sitnik.ru>
2020-03-14 23:22:22 -0400

    Improve type test

--- log/errors.ts
+++ log/errors.ts
@@ -4,3 +4,6 @@ let log = new Log({ nodeId: 'test1', store: new MemoryStore() })
 
 // THROWS '{ name: string; }' is not assignable to parameter of type 'Action'.
 log.add({ name: 'Kate' })
+
+// THROWS Type 'number' is not assignable to type 'string | undefined'.
+log.add({ type: 'user/add', name: 'Kate' }, { id: 1 })
--- log/types.ts
+++ log/types.ts
@@ -3,3 +3,5 @@ import { Log, MemoryStore } from '..'
 let log = new Log({ nodeId: 'test1', store: new MemoryStore() })
 
 log.add({ type: 'user/add', name: 'Kate' })
+
+log.add({ type: 'user/add', name: 'Kate' }, { extra: 1 })
