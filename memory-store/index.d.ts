99ca2efe1ab539cd6946ba6c22ad97b14788c9f4
Andrey Sitnik <andrey@sitnik.ru>
2020-03-22 00:14:31 -0400

    Fix syntax highlight

--- memory-store/index.d.ts
+++ memory-store/index.d.ts
@@ -6,7 +6,7 @@ import { Store } from '../log'
  * It is good for tests, but not for server or client usage,
  * because it store all data in memory and will lose log on exit.
  *
- * ```
+ * ```js
  * import { MemoryStore } from '@logux/core'
  *
  * var log = new Log({
