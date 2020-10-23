8f64080df89036ac5efc019296d855bcac7bc7db
Andrey Sitnik <andrey@sitnik.ru>
2020-10-23 03:02:28 -0400

    Return AnyAction and improve log.type types

--- log/errors.ts
+++ log/errors.ts
@@ -1,16 +1,24 @@
-import { Log, MemoryStore } from '../index.js'
+import { Log, MemoryStore, Action } from '../index.js'
 
 let log = new Log({ nodeId: 'test1', store: new MemoryStore() })
 
-// THROWS '{ name: string; }' is not assignable to parameter of type 'Action
+// THROWS '{ name: string; }' is not assignable to parameter of type 'AnyAction'
 log.add({ name: 'Kate' })
 
 // THROWS Type 'number' is not assignable to type 'string | undefined
 log.add({ type: 'user/add', name: 'Kate' }, { id: 1 })
 
-log.type('user/add', action => {
-  // THROWS 'false' since the types '"user/add"' and '"user/remove"'
-  if (action.type === 'user/remove') {
-    console.log(action)
-  }
+type RenameAction = Action & {
+  type: 'rename'
+  name: string
+}
+
+// THROWS '"rename2"' is not assignable to parameter of type '"rename"'
+log.type<RenameAction>('rename2', action => {
+  document.title = action.name
+})
+
+log.type<RenameAction>('rename', action => {
+  // THROWS 'fullName' does not exist on type 'RenameAction'
+  document.title = action.fullName
 })
--- log/types.ts
+++ log/types.ts
@@ -1,7 +1,20 @@
-import { Log, MemoryStore } from '../index.js'
+import { Log, MemoryStore, Action } from '../index.js'
 
 let log = new Log({ nodeId: 'test1', store: new MemoryStore() })
 
 log.add({ type: 'user/add', name: 'Kate' })
 
 log.add({ type: 'user/add', name: 'Kate' }, { extra: 1 })
+
+type RenameAction = Action & {
+  type: 'rename'
+  name: string
+}
+
+log.type<RenameAction>('rename', action => {
+  document.title = action.name
+})
+
+log.type('rename', action => {
+  console.log(action)
+})
