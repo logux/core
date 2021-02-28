51224ef6cee6b8931ce909e155ecbc0efc2a5481
Eduard Aksamitov <e@edaks.co>
2021-02-28 04:00:41 +0300

    fix(base-node): listener error can be LoguxError

--- base-node/index.d.ts
+++ base-node/index.d.ts
@@ -362,7 +362,7 @@ export class BaseNode<H extends object = {}, L extends Log = Log<Meta>> {
 
   on (
     event: 'error' | 'clientError',
-    listener: (error: Error) => void
+    listener: (error: Error | LoguxError) => void
   ): Unsubscribe
 
   on (
